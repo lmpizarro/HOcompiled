e06feae2ffe92873e356ee624137201e0a263f45
Cuenta Generica de Fisica <wtpc-17@ws12.labo2>
2016-03-09 15:01:44 -0300

    05 multilanguage corrr

--- multilenguaje/05-f-from-c/c-main.c
+++ multilenguaje/05-f-from-c/c-main.c
@@ -1,18 +1,19 @@
-
 #include <stdio.h>
-#include "c-sum.h"
+//#include "c-sum.h"
 
 #define NUM 200
 
 int main(int argc, char **argv)
 {
     int data[NUM], num, i;
+    int sum;
 
     num = NUM;
     for (i=0; i < num; i++) {
         data[i] = i - 100 + 1;  /* NOTE: difference in array numbering */  
     }
 
-    printf("sum=%d\n", sum_abs(data, num));
+    sum_abs_(data, &num, &sum);
+    printf("sum=%d\n", sum);
     return 0;
 }
